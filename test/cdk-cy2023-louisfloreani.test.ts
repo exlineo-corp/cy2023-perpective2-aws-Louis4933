5338d84fc4d0b754239766b02ead47c26ac94712
louis <floreanilo@cy-tech.fr>
2024-02-28 16:32:11 +0100

    Initial commit

---
+++ test/cdk-cy2023-louisfloreani.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCy2023Louisfloreani from '../lib/cdk-cy2023-louisfloreani-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-cy2023-louisfloreani-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCy2023Louisfloreani.CdkCy2023LouisfloreaniStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
