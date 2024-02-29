37d06dca4a253ccc550ce4084732f088c5694246
louis <floreanilo@cy-tech.fr>
2024-02-29 09:09:25 +0100

    Init du repository

--- lib/cdk-cy2023-louisfloreani-stack.ts
+++ lib/cdk-cy2023-louisfloreani-stack.ts
@@ -1,10 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb'; // Table dynamoDB et les attributs de sa clé de partition 
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';// Créer une lambda NodeJS
+
+// Outils Node
+import {join } from 'path'; // Simplifier la gestion des adresses vers les fichiers internes
 
 export class CdkCy2023LouisfloreaniStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Ma stack va créer une table dans DynamoDB
+    const tb = new Table(this, 'tableEvents', {
+      partitionKey: {
+        name: 'event-id',
+        type: AttributeType.STRING
+      },
+      tableName: 'cy-feast-events'
+    });
+    
+    // Créer une lambda
+    const getEventsLambda = new NodejsFunction(this, 'getEvents', {
+      memorySize: 128,
+      description: "Appeler une liste d'évènements",
+      entry:join(__dirname, '../lambda/getEventsLambda.ts'),
+      handler: 'getEventsLambda',
+      environment: {
+        TABLE: tb.tableName
+      }
+    });
 
     // The code that defines your stack goes here
 
