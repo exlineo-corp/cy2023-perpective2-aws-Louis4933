bc7f7801a746d299d680540af9b68aed0118ba34
louis <floreanilo@cy-tech.fr>
2024-03-01 18:08:11 +0100

    Implémentation des opérations crud restantes pour les events

--- lambda/eventsApiHandlerLambda.ts
+++ lambda/eventsApiHandlerLambda.ts
@@ -4,7 +4,7 @@ import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 const db = DynamoDBDocument.from(new DynamoDB());
 const TableName = process.env.TABLE;
 
-exports.handler = async(event:any) => {
+exports.handler = async (event: any) => {
 
     let body;
     let statusCode = 200;
@@ -18,20 +18,21 @@ exports.handler = async(event:any) => {
                 body = await db.scan({ TableName });
                 body = body.Items;
                 break;
-            // case 'POST':
-            //     body = await db.put({ TableName, Item: JSON.parse(event.body) });
-            //     break;
-            // case 'DELETE':
-            //     body = await db.delete({ TableName, Key: JSON.parse(event.body) });
-            //     break;
-            // case 'PUT':
-            //     body = await db.update({ TableName, Key: JSON.parse(event.body) });
-            //     break;
+            case 'POST':
+                body = await db.put({ TableName, Item: JSON.parse(event.body) });
+                break;
+            case 'DELETE':
+                body = await db.delete({ TableName, Key: JSON.parse(event.body) });
+                break;
+            case 'PUT':
+                const { Key, UpdateExpression, ExpressionAttributeNames, ExpressionAttributeValues } = JSON.parse(event.body);
+                body = await db.update({ TableName, Key, UpdateExpression, ExpressionAttributeNames, ExpressionAttributeValues });
+                break;
             default:
                 throw new Error(`Unsupported method "${event.requestContext.httpMethod}"`);
         }
-    } catch (err : any) {
-        statusCode = err.statusCode || 500; 
+    } catch (err: any) {
+        statusCode = err.statusCode || 500;
         body = { error: err.message };
     } finally {
         body = JSON.stringify(body);
--- lib/cdk-cy2023-louisfloreani-stack.ts
+++ lib/cdk-cy2023-louisfloreani-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb'; // Table dynamoDB et les attributs de sa clé de partition 
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';// Créer une lambda NodeJS
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';// Créer une lambda NodeJS
 
 // Outils Node
 import { join } from 'path'; // Simplifier la gestion des adresses vers les fichiers internes
@@ -13,9 +13,9 @@ export class CdkCy2023LouisfloreaniStack extends cdk.Stack {
   eventsAPI: RestApi; // Api du projet
   eventsTb: Table; // Table des events
   getEventsLambda: NodejsFunction; // Lambda pour récupérer les events
-  // postEventsLambda: NodejsFunction; // Lambda pour ajouter un event
-  // deleteEventsLambda: NodejsFunction; // Lambda pour supprimer un event
-  // putEventsLambda: NodejsFunction; // Lambda pour mettre à jour un event
+  postEventsLambda: NodejsFunction; // Lambda pour ajouter un event
+  deleteEventsLambda: NodejsFunction; // Lambda pour supprimer un event
+  putEventsLambda: NodejsFunction; // Lambda pour mettre à jour un event
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -39,46 +39,44 @@ export class CdkCy2023LouisfloreaniStack extends cdk.Stack {
       environment: {
         TABLE: this.eventsTb.tableName
       },
-      runtime:lambda.Runtime.NODEJS_18_X
+      runtime: lambda.Runtime.NODEJS_18_X
     });
 
-    // this.postEventsLambda = new NodejsFunction(this, 'postEvents', {
-    //   memorySize: 128,
-    //   description: "Ajouter un évènement",
-    //   entry: join(__dirname, '../lambda/eventsApiHandlerLambda.ts'),
-    //   environment: {
-    //     TABLE: this.eventsTb.tableName
-    //   },
-    //   runtime:lambda.Runtime.NODEJS_18_X
-    // });
+    this.postEventsLambda = new NodejsFunction(this, 'postEvents', {
+      memorySize: 128,
+      description: "Ajouter un évènement",
+      entry: join(__dirname, '../lambda/eventsApiHandlerLambda.ts'),
+      environment: {
+        TABLE: this.eventsTb.tableName
+      },
+      runtime: lambda.Runtime.NODEJS_18_X
+    });
 
-    // this.deleteEventsLambda = new NodejsFunction(this, 'deleteEvents', {
-    //   memorySize: 128,
-    //   description: "Supprimer un évènement",
-    //   entry: join(__dirname, '../lambda/eventsApiHandlerLambda.ts'),
-    //   handler: 'eventsApiHandlerLambda',
-    //   environment: {
-    //     TABLE: this.eventsTb.tableName
-    //   },
-    //   runtime:lambda.Runtime.NODEJS_18_X
-    // });
+    this.deleteEventsLambda = new NodejsFunction(this, 'deleteEvents', {
+      memorySize: 128,
+      description: "Supprimer un évènement",
+      entry: join(__dirname, '../lambda/eventsApiHandlerLambda.ts'),
+      environment: {
+        TABLE: this.eventsTb.tableName
+      },
+      runtime: lambda.Runtime.NODEJS_18_X
+    });
 
-    // this.putEventsLambda = new NodejsFunction(this, 'putEvents', {
-    //   memorySize: 128,
-    //   description: "Mettre à jour un évènement",
-    //   entry: join(__dirname, '../lambda/eventsApiHandlerLambda.ts'),
-    //   handler: 'eventsApiHandlerLambda',
-    //   environment: {
-    //     TABLE: this.eventsTb.tableName
-    //   },
-    //   runtime:lambda.Runtime.NODEJS_18_X
-    // });
+    this.putEventsLambda = new NodejsFunction(this, 'putEvents', {
+      memorySize: 128,
+      description: "Mettre à jour un évènement",
+      entry: join(__dirname, '../lambda/eventsApiHandlerLambda.ts'),
+      environment: {
+        TABLE: this.eventsTb.tableName
+      },
+      runtime: lambda.Runtime.NODEJS_18_X
+    });
 
     // Donner les permissions pour lire ou écrire dans une table en fonction de la méthode HTTP
     this.eventsTb.grantReadData(this.getEventsLambda);
-    // this.eventsTb.grantWriteData(this.postEventsLambda);
-    // this.eventsTb.grantReadWriteData(this.deleteEventsLambda);
-    // this.eventsTb.grantReadWriteData(this.putEventsLambda);
+    this.eventsTb.grantWriteData(this.postEventsLambda);
+    this.eventsTb.grantReadWriteData(this.deleteEventsLambda);
+    this.eventsTb.grantReadWriteData(this.putEventsLambda);
 
     // Créer une API Gateway
     this.eventsAPI = new RestApi(this, 'eventsAPI', {
@@ -88,23 +86,16 @@ export class CdkCy2023LouisfloreaniStack extends cdk.Stack {
 
     // Intégration des lambdas pour les connecter à la méthode correspondante dans l'API
     const getEventsLambdaIntegration = new LambdaIntegration(this.getEventsLambda);
-    // const postEventsLambdaIntegration = new LambdaIntegration(this.postEventsLambda);
-    // const deleteEventsLambdaIntegration = new LambdaIntegration(this.deleteEventsLambda);
-    // const putEventsLambdaIntegration = new LambdaIntegration(this.putEventsLambda);
-
-    this.eventsAPI.root.addMethod('GET', getEventsLambdaIntegration);
-    // this.eventsAPI.root.addMethod('POST', postEventsLambdaIntegration);
-    // this.eventsAPI.root.addMethod('delete', deleteEventsLambdaIntegration);
-    // this.eventsAPI.root.addMethod('put', putEventsLambdaIntegration);
+    const postEventsLambdaIntegration = new LambdaIntegration(this.postEventsLambda);
+    const deleteEventsLambdaIntegration = new LambdaIntegration(this.deleteEventsLambda);
+    const putEventsLambdaIntegration = new LambdaIntegration(this.putEventsLambda);
 
     // Créer une ressource pour les events
     const apiEvents = this.eventsAPI.root.addResource('events');
 
     apiEvents.addMethod('GET', getEventsLambdaIntegration);
-    // apiEvents.addMethod('POST', postEventsLambdaIntegration);
-    // apiEvents.addMethod('delete', deleteEventsLambdaIntegration);
-    // apiEvents.addMethod('put', putEventsLambdaIntegration);
-
-    // buckets sur les évènements pour mettre les images etc
+    apiEvents.addMethod('POST', postEventsLambdaIntegration);
+    apiEvents.addMethod('DELETE', deleteEventsLambdaIntegration);
+    apiEvents.addMethod('PUT', putEventsLambdaIntegration);
   }
 }
